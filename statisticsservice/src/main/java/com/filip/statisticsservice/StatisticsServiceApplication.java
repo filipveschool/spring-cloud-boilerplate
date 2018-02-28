1bf60e7d9d4911f99376a55eccba5f7f9225cb04
Filip Vanden Eynde <filipve@outlook.com>
2018-02-28 11:08:54 +0100

    add working statistics service

---
+++ statisticsservice/src/main/java/com/filip/statisticsservice/StatisticsServiceApplication.java
@@ -0,0 +1,22 @@
+package com.filip.statisticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class StatisticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsServiceApplication.class, args);
+    }
+}
